04ddc9bcb5f2219f48f88c5dc0773a9e0ddf8aa0
CS <81018394+sahilsindwal@users.noreply.github.com>
2021-05-25 06:14:01 -0700

    Sakura:  Remove duplicate line's

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_KERNEL_CONFIG := sleepy_defconfig
 TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_CLANG_COMPILE := true
 
 # Proton-clang
 TARGET_KERNEL_CLANG_VERSION := proton
