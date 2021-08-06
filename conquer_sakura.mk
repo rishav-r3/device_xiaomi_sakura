993ebf27914d58255f6d7f954c9ca12b35cac84a
“rishav1892” <rishavraj1892@gmail.com>
2021-08-06 12:06:07 +0000

    sakura: Adapt to ConquerOS
    * Also adapt camera hals and drop some dependencies.

--- conquer_sakura.mk
+++ conquer_sakura.mk
@@ -24,18 +24,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-# Inherit some prebuilts
-$(call inherit-product, vendor/custom/prebuilts/config.mk)
-
-# Inherit from mido device
+# Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
-# Inherit some common OctaviOS stuff.
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common ConquerOS stuff.
+$(call inherit-product, vendor/conquer/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
-PRODUCT_NAME := octavi_sakura
+PRODUCT_NAME := conquer_sakura
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 6 Pro
@@ -43,3 +40,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.conquer.device_name=Redmi 6 Pro
