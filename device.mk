9006bd64fb3255ebc3dadfdb6748ffbdf2fbc9e4
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-09-11 20:24:25 +0530

    Sakura: Remove packages at build time

--- device.mk
+++ device.mk
@@ -499,6 +499,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # Don't build debug for host or device
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 ART_BUILD_TARGET_NDEBUG := true
