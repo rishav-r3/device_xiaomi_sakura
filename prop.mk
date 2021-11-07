e7f33ea5cce5fb85b1aa66b15ba19a0e8ae5001c
FrosT2k5 <androygaming@gmail.com>
2021-11-07 15:29:19 +0530

    sakura: Set manufacturer and soc properties
    Set soc model and manufacturer properties

--- prop.mk
+++ prop.mk
@@ -231,6 +231,10 @@ persist.vendor.vt.supported=1 \
 persist.vendor.sys.cnd.iwlan=1 \
 persist.vendor.cne.logging.qxdm=3974
 
+# Soc
+ro.soc.manufacturer=QTI
+ro.soc.model=MSM8953
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true
