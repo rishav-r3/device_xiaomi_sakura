40a52ed1460aa287b236d082442322cb9738bd20
GeoPD <geoemmanuelpd2001@gmail.com>
2021-11-07 10:22:19 +0530

    sakura: Build android.hardware.secure_element@1.2 for qcrild

--- device.mk
+++ device.mk
@@ -399,7 +399,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
     android.hardware.radio.config@1.1 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.2.vendor \
     librmnetctl \
     libcnefeatureconfig \
     libxml2 \
