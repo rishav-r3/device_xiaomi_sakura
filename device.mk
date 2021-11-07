112e5994259d01c6f4c6c1ae222352f4a550f9c6
GeoPD <geoemmanuelpd2001@gmail.com>
2021-11-07 09:46:47 +0530

    sakura: Provide android.system.net.netd@1.1 on vendor for netmgrd

--- device.mk
+++ device.mk
@@ -324,6 +324,10 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     libandroid_net
 
+# Network
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 # Offline charger
 PRODUCT_PACKAGES += \
     charger_res_images \
